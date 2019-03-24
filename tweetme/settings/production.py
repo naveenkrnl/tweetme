988079be1e721c3bfd98d0b95b1ed48df9671d8c
naveen <naveenarorakrnl@gmail.com>
2019-03-25 00:53:29 +0530

    Update Django for whitenoise static

--- tweetme/settings/production.py
+++ tweetme/settings/production.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,4 +131,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static-storage"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
